ecbe115c91fb32a4411abd002b908457d1658458
Hammad Ahmed <qwoo0707@.gmailcom>
2024-02-22 23:58:40 -0800

    Work Complete

---
+++ App.js
@@ -0,0 +1,57 @@
+//chapter 1
+//Q1
+//part1
+
+alert("My name is Hammad.");
+
+//part2
+
+alert("My Last Name is Ahmed.");
+
+//part3
+
+alert("My E-mail Address is qwoo07072@gmail.com.");
+
+//part4
+
+alert("My Phone Number is 0332*****89")
+
+//part5
+
+alert("Please Enter Your Passowad");
+
+//Q2
+
+alert("You`re learning Javascript!");
+
+//Q3 
+
+alert("Welcome to Our Page")
+
+//  Chapter 2
+
+var iphone;
+
+
+//part 3
+var teamname = karachiking ;
+alert("my team name is "+ " "+ teamname)
+
+//chapter 3
+
+var caseQty = 144;
+var num = 9;
+ var total = caseQty * num;
+ console.log(total);
+
+//part 2
+
+var merchtotal = 100;
+var shipingcharge = 10;
+var ordwertotal = merchtotal+shipingcharge;
+console.log(ordwertotal);
+
+//part 3
+
+var a = 10;
+console.log(a+10)
